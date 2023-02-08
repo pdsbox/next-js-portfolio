b6461d94aa5e08ee31233de23640ede20d0ff63e
pdsbox <blutiger009@gmail.com>
2023-02-09 00:18:56 +0900

    tagName change btn -> link in Home Component

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout"
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -9,7 +10,7 @@ export default function Home() {
                         <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">긍정적인 마인드로 개발하는 박대성입니다.
                         </h2>
                         <p className="mb-8 leading-relaxed">회피하기보단 마주하는 용기로, 힘들더라도 참고 견디는 근성으로 문제를 해결에 집중하고 목표로 향하겠습니다.</p>
-                        <button className="inline-flex text-white bg-stone-500 border-0 py-2 px-6 focus:outline-none hover:bg-stone-600 rounded text-lg">Projects &gt;</button>
+                        <Link href="/projects" className="inline-flex text-white bg-stone-500 border-0 py-2 px-6 focus:outline-none hover:bg-stone-600 rounded text-lg">Projects &gt;</Link>
                     </article>
                     <article className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                         <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
