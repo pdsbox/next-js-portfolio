30745aeff0764ab75fbea39ad3e3c58fff08c2f4
pdsbox <blutiger009@gmail.com>
2023-02-09 13:47:35 +0900

    include react-lottie-player and insert Animation

---
+++ components/Home/HomeAnimation.tsx
@@ -0,0 +1,8 @@
+import Animation from "../Animation";
+export default function HomeAnimation() {
+    return (
+        <article className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <Animation />
+        </article>
+    );
+}
