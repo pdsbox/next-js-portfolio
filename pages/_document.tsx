525d96201bd56e595c78226148272762069c5ee1
pdsbox <blutiger009@gmail.com>
2023-02-13 21:44:56 +0900

    Html Head language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="kr">
       <Head>
         <title>박대성 포트폴리오</title>
         <meta name="description" content="박대성의 포트폴리오" />
