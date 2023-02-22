9ba6be726da6112f7f3d13fc69e3706e1a48608c
pdsbox <blutiger009@gmail.com>
2023-02-22 17:55:22 +0900

    Head tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="description" content="박대성의 포트폴리오" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/icon16.png" />
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"></link>
+
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ export default function Document() {
   return (
     <Html lang="kr">
       <Head>
-
+        <link rel="icon" href="/icon16.png" />
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"></link>
       </Head>
       <body>
         <Main />
