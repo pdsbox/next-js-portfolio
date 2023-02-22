cbb6ab1879261211e4e4a483e6e347de5c906eb1
pdsbox <blutiger009@gmail.com>
2023-02-22 17:39:20 +0900

    text

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <Image src={icon} alt="img" width="20" quality={100} />
-                    <span className="ml-3 text-xl">박대성 포트폴리오 페이지입니다.</span>
+                    <span className="ml-3 text-xl">박대성 포트폴리오</span>
                 </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
--- components/Home/HomeText.tsx
+++ components/Home/HomeText.tsx
@@ -2,12 +2,17 @@ import Link from "next/link";
 export default function HomeText() {
     return (
         <article className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h2 className="title-font sm:text-4xl text-3xl mb-6 font-medium text-neutral-800">긍정적인 마인드로 개발하는 박대성입니다.
+            <h2 className="title-font sm:text-4xl text-3xl mb-6 font-medium text-neutral-800">긍정적이고 개방적인 문제해결사
             </h2>
-            <p className="mb-9 leading-relaxed text-neutral-600">회피하기보단 마주하는 용기로.<br />
-                힘들더라도 참고 견디는 근성으로.<br />
-                문제 해결에 집중하며 목표를 달성하겠습니다.</p>
-            <Link href="/projects" className="inline-flex text-green-100 text-base bg-neutral-400 border-0 py-2 px-6 focus:outline-none hover:bg-neutral-500 rounded">Welcome &gt;</Link>
+            <p className="mb-9 leading-relaxed text-neutral-600">
+                여러 사람들과 함께 문제를 해결하가는 과정을 즐깁니다.<br />
+                각자의 방식대로 각자의 의견을 나누다보면,<br />
+                매우 다양하고 흥미로운 아이디어들을 발견합니다.<br />
+                이것을 구체화시키고 적용하면서 문제를 해결하다보면,<br />
+                '또 경험하고 싶다, 더 다양한 문제를 해결하고 싶다.'<br />
+                성취감과 동기부여가 생깁니다.
+            </p>
+            <Link href="/projects" className="inline-flex text-green-100 text-base bg-neutral-400 border-0 py-2 px-6 focus:outline-none hover:bg-neutral-500 rounded">Projects &gt;</Link>
         </article>
     );
 }
