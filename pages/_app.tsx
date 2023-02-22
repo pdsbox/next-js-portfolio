9f22b6c290fd432e9cf2f117512dd3ddb2382c3f
pdsbox <blutiger009@gmail.com>
2023-02-22 17:50:57 +0900

    meta tags dir change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="description" content="박대성의 포트폴리오" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon16.png" />
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,7 @@ export default function Document() {
   return (
     <Html lang="kr">
       <Head>
-        <meta name="description" content="박대성의 포트폴리오" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/icon16.png" />
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"></link>
+
       </Head>
       <body>
         <Main />
